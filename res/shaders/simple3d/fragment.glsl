349b581cdd386d3c33d5ff1f16bc704d180b8aa1
lucasgpulcinelli <lucasgpulcinelli@users.noreply.github.com>
2023-10-28 17:20:48 -0300

    solving error with texture Y values

--- res/shaders/simple3d/fragment.glsl
+++ res/shaders/simple3d/fragment.glsl
@@ -7,7 +7,7 @@ out vec4 fragColor;
 uniform sampler2D Texture;
 
 void main(){
-  fragColor = texture(Texture, frag_texture);
+  fragColor = texture(Texture, vec2(frag_texture.x, 1-frag_texture.y));
 //  fragColor = vec4(frag_texture.x, frag_texture.y, 0, 1);
 }
 
--- src/Mesh.cpp
+++ src/Mesh.cpp
@@ -146,8 +146,8 @@ int loadTexture(std::string texture_file) {
   glGenTextures(1, &texture);
   glBindTexture(GL_TEXTURE_2D, texture);
 
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER,
                   GL_LINEAR_MIPMAP_LINEAR);
 
